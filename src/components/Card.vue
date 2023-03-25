dbbbb784e42cc61be5018ccb010b11873a30cc57
AbdelrhmanAbuelnile <abdohani362@gmail.com>
2023-03-25 03:12:23 +0200

    fixed id and sizes

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,12 +1,12 @@
 <template>
-    <div class="flex justify-center items-center flex-col gap-4 p-5 rounded-xl bg-Dark-Grayish-Blue max-w-lg">
-        <h1 class="text-Neon-Green">Advice #{{ slip_id }}</h1>
+    <div class="flex justify-center items-center w-5/6 h-[300px] flex-col gap-4 p-5 rounded-xl bg-Dark-Grayish-Blue max-w-lg">
+        <h1 class="text-Neon-Green">Advice #{{ id }}</h1>
         <p class="text-Light-Cyan text-2.5xl text-center">"{{ advice }}"</p>
-        <img src="../assets/images/pattern-divider-mobile.svg" alt="">
+        <img src="../assets/images/pattern-divider-mobile.svg" class="w-full" alt="">
         <button class="bg-Neon-Green p-3 rounded-full" @click="fetchAdvice" >
             <img
                 src="../assets/images/icon-dice.svg"
-                alt=""
+                alt="dice icon"
             >
             
         </button>
@@ -18,12 +18,14 @@
     import axios from 'axios';
 
     const advice = ref(null);
+    const id = ref(null)
     const i = ref(1)
     const fetchAdvice = () => {
         axios
         .get("https://api.adviceslip.com/advice")
         .then(response => {
         advice.value = response.data.slip.advice;
+        id.value = response.data.slip.id;
         console.log(advice.value)
         })
         .catch(error => {
